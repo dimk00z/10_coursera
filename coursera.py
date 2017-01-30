a26026bff2c7d29c6f02afb2e1f2d85405c35fe8
Dmitry <dimk00z@gmail.com>
2017-01-30 14:02:41 +0300

    Исправления ошибок - разбил функции
    исправил(надуюсь) названия переменных

--- coursera.py
+++ coursera.py
@@ -6,51 +6,57 @@ from openpyxl import Workbook
 
 def get_courses_list_from_url(url):
     courses_tree = etree.fromstring(requests.get(url).content)
-    courses_list = []
-    for cours in courses_tree:
-        courses_list.append(cours.getchildren()[0].text)
-    return courses_list
+    return [course.getchildren()[0].text
+            for course in courses_tree]
 
 
-def get_course_info(course_url):
-    course_html = requests.get(course_url).content
-    soup = BeautifulSoup(course_html, 'html.parser')
-    title = soup.find('div', {'class': 'title'}).text
-    language_info = soup.find('div', {'class': 'language-info'}).text
-    startdate = soup.find('div', {'class': 'startdate'}).text
+def parser_course_page(course_content):
+    course_info = {}
+    soup = BeautifulSoup(course_content, 'html.parser')
+    course_info['title'] = soup.find('div', {'class': 'title'}).text
+    course_info['language_info'] = soup.find(
+        'div', {'class': 'language-info'}).text
+    course_info['startdate'] = soup.find('div', {'class': 'startdate'}).text
     weeks = soup.find('div', {'class': 'rc-WeekView'})
-    if weeks:
-        weeks = len(weeks)
+    course_info['weeks'] = len(weeks) if weeks else ''
     ratings = soup.find('div', {'class': 'ratings-text'})
-    if ratings:
-        ratings = ratings.text
-    return title, language_info, startdate, weeks, ratings
+    course_info['ratings'] = ratings.text if ratings else ''
+    return course_info
 
 
-def output_courses_info_to_xlsx(courses_info,
-                                filepath='output.xlsx',
-                                courses_count=20):
+def get_courses_info(courses_list, courses_count=20):
+    return [parser_course_page(requests.get(course_url).content)
+            for course_url in courses_list[:courses_count]]
+
+
+def get_courses_rows(courses_info):
+    courses_rows = []
+    courses_rows.append(['Course name', 'Language', 'Start date',
+                         'Number of weeks', 'Rating'])
+    for course_info in courses_info:
+        courses_rows.append([course_info['title'],
+                             course_info['language_info'],
+                             course_info['startdate'],
+                             course_info['weeks'],
+                             course_info['ratings']])
+    return courses_rows
+
+
+def output_courses_info_to_xlsx(courses_rows,
+                                filepath):
     xlsx_book = Workbook()
     sheet = xlsx_book.active
-    sheet['A1'] = 'Course name'
-    sheet['B1'] = 'Language'
-    sheet['C1'] = 'Start date'
-    sheet['D1'] = 'Number of weeks'
-    sheet['E1'] = 'Rating'
-    for course_number, course in enumerate(courses_list[:courses_count]):
-        course_info = get_course_info(course)
-        string_number = str(course_number + 2)
-        sheet['A' + string_number] = course_info[0]
-        sheet['B' + string_number] = course_info[1]
-        sheet['C' + string_number] = course_info[2]
-        if course_info[3]:
-            sheet['D' + string_number] = course_info[3]
-        if course_info[4]:
-            sheet['E' + string_number] = course_info[4]
+    for row in courses_rows:
+        sheet.append(row)
     xlsx_book.save(filepath)
+    return filepath
 
 
 if __name__ == '__main__':
-    url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    courses_list = get_courses_list_from_url(url)
-    output_courses_info_to_xlsx(courses_list)
+    coursera_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    xlsx_filepath = 'output.xlsx'
+    courses_list = get_courses_list_from_url(coursera_url)
+    courses_info = get_courses_info(courses_list)
+    courses_rows = get_courses_rows(courses_info)
+    output_courses_info_to_xlsx(courses_rows, xlsx_filepath)
+    print("Данные сохранены в {}".format(xlsx_filepath))
