22b3756d2e17461566051b47e473cce54b37a3e4
Dmitry <dimk00z@gmail.com>
2017-02-25 09:41:10 +0300

    Ошибки
    Исправил readme.md. Удалил "грязную" функцию, добавил константу

--- coursera.py
+++ coursera.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
 
+COURSERA_URL = 'https://www.coursera.org/sitemap~www~courses.xml'
+
+
 def get_courses_list_from_url(url):
     courses_tree = etree.fromstring(requests.get(url).content)
     return [course.getchildren()[0].text
@@ -13,10 +16,13 @@ def get_courses_list_from_url(url):
 def parser_course_page(course_content):
     course_info = {}
     soup = BeautifulSoup(course_content, 'html.parser')
-    course_info['title'] = soup.find('div', {'class': 'title'}).text
-    course_info['language_info'] = soup.find(
-        'div', {'class': 'language-info'}).text
-    course_info['startdate'] = soup.find('div', {'class': 'startdate'}).text
+    title = soup.find('h1', {'class': 'title display-3-text'})
+    course_info['title'] = title.text if title else ''
+    language_info = soup.find(
+        'div', {'class': 'language-info'})
+    course_info['language_info'] = language_info.text if language_info else ''
+    startdate = soup.find('div', {'class': 'startdate'})
+    course_info['startdate'] = startdate.text if startdate else ''
     weeks = soup.find('div', {'class': 'rc-WeekView'})
     course_info['weeks'] = len(weeks) if weeks else ''
     ratings = soup.find('div', {'class': 'ratings-text'})
@@ -24,15 +30,12 @@ def parser_course_page(course_content):
     return course_info
 
 
-def get_courses_info(courses_list, courses_count=20):
-    return [parser_course_page(requests.get(course_url).content)
-            for course_url in courses_list[:courses_count]]
-
-
-def get_courses_rows(courses_info):
+def get_courses_rows(courses_list, courses_count=20):
     courses_rows = []
     courses_rows.append(['Course name', 'Language', 'Start date',
                          'Number of weeks', 'Rating'])
+    courses_info = [parser_course_page(requests.get(course_url).content)
+                    for course_url in courses_list[:courses_count]]
     for course_info in courses_info:
         courses_rows.append([course_info['title'],
                              course_info['language_info'],
@@ -43,7 +46,7 @@ def get_courses_rows(courses_info):
 
 
 def output_courses_info_to_xlsx(courses_rows,
-                                filepath):
+                                filepath='output.xlsx'):
     xlsx_book = Workbook()
     sheet = xlsx_book.active
     for row in courses_rows:
@@ -53,10 +56,7 @@ def output_courses_info_to_xlsx(courses_rows,
 
 
 if __name__ == '__main__':
-    coursera_url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    xlsx_filepath = 'output.xlsx'
-    courses_list = get_courses_list_from_url(coursera_url)
-    courses_info = get_courses_info(courses_list)
-    courses_rows = get_courses_rows(courses_info)
-    output_courses_info_to_xlsx(courses_rows, xlsx_filepath)
-    print("Данные сохранены в {}".format(xlsx_filepath))
+    courses_list = get_courses_list_from_url(COURSERA_URL)
+    courses_rows = get_courses_rows(courses_list)
+    out_file_name = output_courses_info_to_xlsx(courses_rows)
+    print("Данные сохранены в {}".format(out_file_name))
